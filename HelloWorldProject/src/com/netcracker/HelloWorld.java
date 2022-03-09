621ab8012292b67e3d39d620cedf74d193095e25
hardcatikus <97665410+hardcatikus@users.noreply.github.com>
2022-03-09 21:20:27 +0300

    Add files via upload

---
+++ HelloWorldProject/src/com/netcracker/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.netcracker;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        for(String str : args){
+            System.out.println(str);
+        }
+        //System.out.println("Hello World");
+    }
+}
